d8eb5d63d1c1c49faf6f12741f0b3d8e049e03b7
mamadjonovKO <mamadjonov_k@auca.kg>
2024-10-25 13:07:23 +0600

    Refactored based on the review

--- src/main/java/faang/school/postservice/aspect/AnalyticsEventAspect.java
+++ src/main/java/faang/school/postservice/aspect/AnalyticsEventAspect.java
@@ -3,6 +3,7 @@ package faang.school.postservice.aspect;
 import faang.school.postservice.config.context.UserContext;
 import faang.school.postservice.event.AnalyticsEvent;
 import faang.school.postservice.event.EventType;
+import faang.school.postservice.event.PostViewEventDto;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.publisher.PostViewEventPublisher;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Aspect
 @Component
@@ -22,13 +25,22 @@ public class AnalyticsEventAspect {
     public void publishPostViewEvent(JoinPoint joinPoint, AnalyticsEvent analyticsEvent, Post post) {
         if (analyticsEvent.value() == EventType.POST_VIEW) {
             Long viewerId = getViewerId();
-            postViewEventPublisher.publish(post, viewerId);
+            PostViewEventDto event = new PostViewEventDto(
+                    post.getId(),
+                    getAuthorOrProjectId(post),
+                    viewerId,
+                    LocalDateTime.now()
+            );
+            postViewEventPublisher.publish(event);
         }
     }
 
     private Long getViewerId() {
         userContext.setUserId(1);
-        Long viewerId = userContext.getUserId();
-        return viewerId;
+        return userContext.getUserId();
+    }
+
+    private Long getAuthorOrProjectId(Post post) {
+        return post.getAuthorId() != null ? post.getAuthorId() : post.getProjectId();
     }
 }
--- src/main/java/faang/school/postservice/publisher/PostViewEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/PostViewEventPublisher.java
@@ -4,39 +4,25 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.config.redis.RedisProperties;
 import faang.school.postservice.event.PostViewEventDto;
-import faang.school.postservice.model.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Service
 @RequiredArgsConstructor
-public class PostViewEventPublisher {
+public class PostViewEventPublisher implements EventPublisher<PostViewEventDto> {
 
     private final RedisTemplate<String, Object> redisTemplate;
     private final RedisProperties redisProperties;
     private final ObjectMapper objectMapper;
 
-    public void publish(Post post, Long viewerId) {
-        Long authorId = getAuthorOrProjectId(post);
-        PostViewEventDto event = new PostViewEventDto(
-                post.getId(),
-                authorId,
-                viewerId,
-                LocalDateTime.now()
-        );
-
+    @Override
+    public void publish(PostViewEventDto event) {
         try {
             String eventAsJson = objectMapper.writeValueAsString(event);
             redisTemplate.convertAndSend(redisProperties.getPostViewChannel(), eventAsJson);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to serialize PostViewEvent", e);
         }
-    }
-
-    private Long getAuthorOrProjectId(Post post) {
-        return post.getAuthorId() != null ? post.getAuthorId() : post.getProjectId();
     }
 }
