34728f573d8b800273fadb6967af7d55a22c4586
Andrew Balashov <balashov83@gmail.com>
2024-11-11 21:28:02 +0300

    Feat: gets Post by id
    PostService: getPostById method added

---
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -0,0 +1,18 @@
+package faang.school.postservice.service;
+
+import faang.school.postservice.model.Post;
+import faang.school.postservice.repository.PostRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private PostRepository postRepository;
+
+    public Post getPostById(Long id) {
+        return postRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException(String.format("Post with id #%s not found", id)));
+    }
+}
