45e8df62db5d666b5e5d6aec378b85b699e70ed9
Andrew Balashov <balashov83@gmail.com>
2024-11-12 00:48:18 +0300

    Feat: validates post exists by id
    PostValidator: validatePostExistsById method added

---
+++ src/main/java/faang/school/postservice/validator/PostValidator.java
@@ -0,0 +1,18 @@
+package faang.school.postservice.validator;
+
+import faang.school.postservice.repository.PostRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostValidator {
+    private PostRepository postRepository;
+
+    public void validatePostExistsById(Long postId) {
+        if (!postRepository.existsById(postId)) {
+            throw new EntityNotFoundException(String.format("Post with id #%s doesn't exist", postId));
+        }
+    }
+}
