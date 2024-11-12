c9af9ac9ea7e60bb9c15e10c78af0034ef7e6da2
Roman Nifontov <romannif@mail.ru>
2024-11-12 22:53:43 +0500

    Добавил классы заглушки, так как непонятки с удаленными файлами

---
+++ src/main/java/faang/school/postservice/dto/payment/PaymentRequest.java
@@ -0,0 +1,6 @@
+package faang.school.postservice.dto.payment;
+
+public class PaymentRequest {
+
+    //класс заглушка
+}
---
+++ src/main/java/faang/school/postservice/dto/payment/PaymentResponse.java
@@ -0,0 +1,6 @@
+package faang.school.postservice.dto.payment;
+
+public class PaymentResponse {
+
+    //класс заглушка
+}
