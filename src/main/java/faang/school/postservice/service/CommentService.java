788e664e3ec309d3fb57c05de59e58a4b2e914fe
Andrew Balashov <balashov83@gmail.com>
2024-11-12 16:15:28 +0300

    Feat: adds update logic in service
    CommentService: updateComment added

--- src/main/java/faang/school/postservice/service/CommentService.java
+++ src/main/java/faang/school/postservice/service/CommentService.java
@@ -3,10 +3,13 @@ package faang.school.postservice.service;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.dto.comment.CreateCommentDto;
+import faang.school.postservice.dto.comment.UpdateCommentDto;
 import faang.school.postservice.mapper.CommentMapper;
 import faang.school.postservice.model.Comment;
 import faang.school.postservice.repository.CommentRepository;
+import faang.school.postservice.validator.CommentValidator;
 import faang.school.postservice.validator.PostValidator;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,9 +22,10 @@ public class CommentService {
     private final PostService postService;
     private final CommentMapper commentMapper;
     private final PostValidator postValidator;
+    private final CommentValidator commentValidator;
     private final UserServiceClient userServiceClient;
 
-    public CommentDto create(long postId, CreateCommentDto dto) {
+    public CommentDto createComment(long postId, CreateCommentDto dto) {
         postValidator.validatePostExistsById(postId);
         userServiceClient.getUser(dto.getAuthorId());
 
@@ -31,5 +35,24 @@ public class CommentService {
         log.info("New comment #{} post #{} has been created", comment.getId(), comment.getPost().getId());
 
         return commentMapper.toDto(comment);
+    }
+
+    public CommentDto updateComment(long postId, UpdateCommentDto dto) {
+        postValidator.validatePostExistsById(postId);
+        commentValidator.validateCommentExistsById(dto.getId());
+        userServiceClient.getUser(dto.getAuthorId());
+
+        Comment comment = getCommentById(dto.getId());
+        commentValidator.validateCommentAuthorId(comment, dto.getAuthorId());
+        comment.setContent(dto.getContent());
+        commentRepository.save(comment);
+        log.info("Comment #{} to post #{} has been updated", comment.getId(), comment.getPost().getId());
+
+        return commentMapper.toDto(comment);
+    }
+
+    public Comment getCommentById(Long id) {
+        return commentRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException(String.format("Comment with id #%s doesn't exist", id)));
     }
 }
--- src/test/java/faang/school/postservice/service/CommentServiceTest.java
+++ src/test/java/faang/school/postservice/service/CommentServiceTest.java
@@ -3,10 +3,12 @@ package faang.school.postservice.service;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.dto.comment.CreateCommentDto;
+import faang.school.postservice.dto.comment.UpdateCommentDto;
 import faang.school.postservice.mapper.CommentMapper;
 import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.CommentRepository;
+import faang.school.postservice.validator.CommentValidator;
 import faang.school.postservice.validator.PostValidator;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -36,12 +40,16 @@ class CommentServiceTest {
     PostValidator postValidator;
 
     @Mock
+    CommentValidator commentValidator;
+
+    @Mock
     UserServiceClient userServiceClient;
 
     @InjectMocks
     CommentService commentService;
 
     CreateCommentDto dto;
+    UpdateCommentDto updateDto;
     CommentDto responseDto;
     Post post;
     Comment comment;
@@ -49,21 +57,22 @@ class CommentServiceTest {
 
     @BeforeEach
     void setUp() {
-        dto = createTestCreateDto();
-        responseDto = createTestCommentDto();
-        post = createTestPost();
-        comment = createTestComment();
+        dto = createTestCreateCommentDto();
+        updateDto = createTestUpdateCommentDto();
+        responseDto = createCommentTestCommentDto();
+        post = createCommentTestPost();
+        comment = createCommentTestComment();
     }
 
     @Test
     @DisplayName("Create comment success")
-    void testCreateCommentSuccess() {
+    void testCreateCommentCommentSuccess() {
 
         when(commentMapper.toEntity(dto)).thenReturn(comment);
         when(postService.getPostById(postId)).thenReturn(post);
         when(commentMapper.toDto(comment)).thenReturn(responseDto);
 
-        CommentDto result = commentService.create(postId, dto);
+        CommentDto result = commentService.createComment(postId, dto);
 
         verify(postValidator, times(1)).validatePostExistsById(postId);
         verify(userServiceClient, times(1)).getUser(dto.getAuthorId());
@@ -76,11 +85,11 @@ class CommentServiceTest {
 
     @Test
     @DisplayName("Create comment fail: invalid post id")
-    void testCreateCommentFailInvalidPostId() {
+    void testCreateCommentCommentFailInvalidPostId() {
         doThrow(EntityNotFoundException.class).when(postValidator).validatePostExistsById(postId);
 
 
-        assertThrows(EntityNotFoundException.class, () -> commentService.create(postId, dto));
+        assertThrows(EntityNotFoundException.class, () -> commentService.createComment(postId, dto));
 
         verify(postValidator, times(1)).validatePostExistsById(postId);
         verify(userServiceClient, never()).getUser(dto.getAuthorId());
@@ -92,10 +101,10 @@ class CommentServiceTest {
 
     @Test
     @DisplayName("Create comment fail: invalid author id")
-    void testCreateCommentFailInvalidAuthorId() {
+    void testCreateCommentCommentFailInvalidAuthorId() {
         doThrow(EntityNotFoundException.class).when(userServiceClient).getUser(dto.getAuthorId());
 
-        assertThrows(EntityNotFoundException.class, () -> commentService.create(postId, dto));
+        assertThrows(EntityNotFoundException.class, () -> commentService.createComment(postId, dto));
 
         verify(postValidator, times(1)).validatePostExistsById(postId);
         verify(userServiceClient, times(1)).getUser(dto.getAuthorId());
@@ -104,14 +113,63 @@ class CommentServiceTest {
         verify(commentMapper, never()).toDto(any(Comment.class));
     }
 
-    private CreateCommentDto createTestCreateDto() {
+    @Test
+    @DisplayName("Update comment success")
+    void testUpdateCommentSuccess() {
+        comment.setPost(post);
+        when(commentRepository.findById(updateDto.getId())).thenReturn(Optional.of(comment));
+        when(commentMapper.toDto(comment)).thenReturn(responseDto);
+
+        CommentDto result = commentService.updateComment(postId, updateDto);
+
+        verify(postValidator, times(1)).validatePostExistsById(postId);
+        verify(commentValidator, times(1)).validateCommentExistsById(updateDto.getId());
+        verify(userServiceClient, times(1)).getUser(updateDto.getAuthorId());
+        verify(commentValidator, times(1)).validateCommentAuthorId(comment, updateDto.getAuthorId());
+        verify(commentRepository, times(1)).save(comment);
+
+        assertNotNull(result);
+        assertEquals(1L, result.getId());
+        assertEquals("Test content", result.getContent());
+    }
+
+    @Test
+    @DisplayName("Update comment fail: invalid post id")
+    void testUpdateCommentFailInvalidPostId() {
+        doThrow(EntityNotFoundException.class).when(postValidator).validatePostExistsById(postId);
+
+        assertThrows(EntityNotFoundException.class, () -> commentService.updateComment(postId, updateDto));
+
+        verify(postValidator, times(1)).validatePostExistsById(postId);
+        verify(commentValidator, never()).validateCommentExistsById(updateDto.getId());
+        verify(userServiceClient, never()).getUser(updateDto.getAuthorId());
+        verify(commentValidator, never()).validateCommentAuthorId(comment, updateDto.getAuthorId());
+        verify(commentRepository, never()).save(comment);
+    }
+
+    @Test
+    @DisplayName("Update comment fail: invalid comment id")
+    void testUpdateCommentFailInvalidCommentId() {
+        when(commentRepository.findById(updateDto.getId())).thenReturn(Optional.empty());
+
+        Exception ex =assertThrows(EntityNotFoundException.class, () -> commentService.updateComment(postId, updateDto));
+        assertEquals("Comment with id #3 doesn't exist", ex.getMessage());
+
+        verify(postValidator, times(1)).validatePostExistsById(postId);
+        verify(commentValidator, times(1)).validateCommentExistsById(updateDto.getId());
+        verify(userServiceClient, times(1)).getUser(updateDto.getAuthorId());
+        verify(commentValidator, never()).validateCommentAuthorId(comment, updateDto.getAuthorId());
+        verify(commentRepository, never()).save(comment);
+    }
+
+    private CreateCommentDto createTestCreateCommentDto() {
         return CreateCommentDto.builder()
                 .authorId(1L)
                 .content("Test content")
                 .build();
     }
 
-    private CommentDto createTestCommentDto() {
+    private CommentDto createCommentTestCommentDto() {
         return CommentDto.builder()
                 .id(1L)
                 .content("Test content")
@@ -123,7 +181,15 @@ class CommentServiceTest {
                 .build();
     }
 
-    private Post createTestPost() {
+    private UpdateCommentDto createTestUpdateCommentDto() {
+        return UpdateCommentDto.builder()
+                .id(3L)
+                .authorId(1L)
+                .content("Test update content")
+                .build();
+    }
+
+    private Post createCommentTestPost() {
         return Post.builder()
                 .id(1L)
                 .content("Test content")
@@ -131,7 +197,7 @@ class CommentServiceTest {
                 .build();
     }
 
-    private Comment createTestComment() {
+    private Comment createCommentTestComment() {
         return Comment.builder()
                 .id(1L)
                 .content("Test content")
