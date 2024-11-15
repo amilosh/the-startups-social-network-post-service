9beb09fb4301e31e83ad144b8829ba15978d2e86
Alena Shmelkova <AlenaShmelkova@gmail.com>
2024-11-15 03:02:38 -0500

    Added Annotations to the Controller

--- src/main/java/faang/school/postservice/controller/CommentController.java
+++ src/main/java/faang/school/postservice/controller/CommentController.java
@@ -8,6 +8,13 @@ import faang.school.postservice.service.comment.CommentService;
 import faang.school.postservice.validator.CommentValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,25 +22,30 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api")
 public class CommentController {
     private final CommentService commentService;
     private final CommentValidator commentValidator;
 
-    public CommentDtoOutput create(CommentDtoInput input) {
+    @PostMapping("/comments")
+    public CommentDtoOutput create(@RequestBody CommentDtoInput input) {
         commentValidator.validateCommentDtoInput(input);
         return commentService.createComment(input);
     }
 
-    public CommentDtoOutputUponUpdate update(CommentUpdateDto updatingInput) {
+    @PostMapping("comments")
+    public CommentDtoOutputUponUpdate update(@RequestBody CommentUpdateDto updatingInput) {
         commentValidator.validateCommentUpdateDto(updatingInput);
         return commentService.updateComment(updatingInput);
     }
 
-    public List<CommentDtoOutput> getCommentsByPostId(Long postId) {
+    @GetMapping("/comments")
+    public List<CommentDtoOutput> getCommentsByPostId(@RequestParam("postId") Long postId) {
         return commentService.getCommentsByPostId(postId);
     }
 
-    public void deleteComment(Long commentId) {
+    @DeleteMapping("/comments/{commentId}")
+    public void deleteComment(@PathVariable("commentId") Long commentId) {
         commentService.deleteComment(commentId);
     }
 }
