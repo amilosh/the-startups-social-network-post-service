1b11e959b5bfc4153eb6053efd3ecca136a6277c
daurenberdibekov <berder08@gmail.com>
2024-11-15 15:15:05 +0100

    Refactored like comment mapper

--- src/main/java/faang/school/postservice/mapper/like/LikeCommentMapper.java
+++ src/main/java/faang/school/postservice/mapper/like/LikeCommentMapper.java
@@ -2,20 +2,11 @@ package faang.school.postservice.mapper.like;
 
 import faang.school.postservice.dto.like.LikeCommentDto;
 import faang.school.postservice.model.Like;
-import faang.school.postservice.model.Post;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface LikeCommentMapper {
 
-//    @Mapping(source = "like.id", target = "id")
-//    @Mapping(source = "like.userId", target = "userId")
-//    @Mapping(source = "post.id", target = "postId")
-//    @Mapping(source = "like.comment.id", target = "commentId")
-//    LikeCommentDto toDto(Like like, Post post);
-
     Like toEntity(LikeCommentDto likeCommentDto);
-
 }
