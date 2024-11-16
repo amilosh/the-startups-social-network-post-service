848499ab77c6ee1c8c2bcb3e93c5b658c4047098
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-11-17 01:44:11 +0500

    BJS2-40813 Task
    Added LikeController and LikeService.
    
    With Tests (version 1.0)

--- src/main/java/faang/school/postservice/controller/LikeController.java
+++ src/main/java/faang/school/postservice/controller/LikeController.java
@@ -2,10 +2,10 @@ package faang.school.postservice.controller;
 
 import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.service.LikeService;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +15,19 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@Validated
 public class LikeController {
     private final LikeService likeService;
 
-    @GetMapping("/post/{id}/likes")
-    public List<UserDto> getUsersWhoLikePostByPostId(@Valid @PathVariable @Positive long id) {
-        log.info("Request for users by post id: {}", id);
-        return likeService.getUsersWhoLikePostByPostId(id);
+    @GetMapping("/post/{postId}/likes")
+    public List<UserDto> getUsersWhoLikePostByPostId(@PathVariable @Positive long postId) {
+        log.info("Request for users by post id: {}", postId);
+        return likeService.getUsersWhoLikePostByPostId(postId);
     }
 
-    @GetMapping("/comment/{id}/likes")
-    public List<UserDto> getCommentLikers(@Valid @PathVariable @Positive long id) {
-        log.info("Request for users by comment id: {}", id);
-        return likeService.getCommentLikers(id);
+    @GetMapping("/comment/{commentId}/likes")
+    public List<UserDto> getUsersWhoLikeComments(@PathVariable @Positive long commentId) {
+        log.info("Request for users by comment id: {}", commentId);
+        return likeService.getUsersWhoLikeComments(commentId);
     }
 }
--- src/main/java/faang/school/postservice/service/LikeService.java
+++ src/main/java/faang/school/postservice/service/LikeService.java
@@ -4,36 +4,32 @@ import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.model.Like;
 import faang.school.postservice.repository.LikeRepository;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Validated
 public class LikeService {
     private static final int BATCH_SIZE = 100;
     private final LikeRepository likeRepository;
     private final UserServiceClient userServiceClient;
 
-    public List<UserDto> getUsersWhoLikePostByPostId(@NotNull @Positive long Id) {
+    public List<UserDto> getUsersWhoLikePostByPostId(long Id) {
         List<Like> usersWhoLikedPost  = likeRepository.findByPostId(Id);
-        return getUserDtos(usersWhoLikedPost);
+        return mapLikesToUserDtos(usersWhoLikedPost);
     }
 
 
-    public List<UserDto> getCommentLikers(@NotNull @Positive long id) {
+    public List<UserDto> getUsersWhoLikeComments(long id) {
         List<Like> usersWhoLikedComment  = likeRepository.findByCommentId(id);
-        return getUserDtos(usersWhoLikedComment);
+        return mapLikesToUserDtos(usersWhoLikedComment);
     }
 
 
-    private List<UserDto> getUserDtos(@NotNull @Positive List<Like> usersWhoLiked) {
+    private List<UserDto> mapLikesToUserDtos(List<Like> usersWhoLiked) {
         List<Long> userIds = usersWhoLiked.stream()
                 .map(Like::getUserId)
                 .toList();
@@ -51,7 +47,6 @@ public class LikeService {
             List<UserDto> batchResults = userServiceClient.getUsersByIds(batch);
             userDtos.addAll(batchResults);
         }
-
         return userDtos;
     }
 }
