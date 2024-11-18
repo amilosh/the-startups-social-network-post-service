f5c77c1debc6f4ce83ab324bd13ccfcf2d5a6b9f
Dmytro Yevsieienko <youdima70@gmail.com>
2024-11-19 00:53:32 +0200

    Рефакторинг

--- src/main/java/faang/school/postservice/controller/CommentController.java
+++ src/main/java/faang/school/postservice/controller/CommentController.java
@@ -2,7 +2,6 @@ package faang.school.postservice.controller;
 
 import faang.school.postservice.dto.CommentDto;
 import faang.school.postservice.service.comment.CommentService;
-import faang.school.postservice.validator.comment.CommentControllerValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
