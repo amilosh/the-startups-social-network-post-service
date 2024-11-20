91915638ca04f95c466b7e6ec3db1ec491dbb353
ifef228 <ifef228@gmail.com>
2024-11-20 17:17:28 +0300

    sql for feed

--- src/main/java/faang/school/postservice/repository/UserRepository.java
+++ src/main/java/faang/school/postservice/repository/UserRepository.java
@@ -2,6 +2,16 @@ package faang.school.postservice.repository;
 
 import faang.school.postservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(nativeQuery = true,
+            value = "select p.id from subscription s" +
+                    " join post p on s.followee_id = p.author_id where" +
+                    " s.follower_id = :userId order by p.published_at;"
+    )
+    List<Long> getPostIdsForFeedById(@Param("userId") long userId);
 }
