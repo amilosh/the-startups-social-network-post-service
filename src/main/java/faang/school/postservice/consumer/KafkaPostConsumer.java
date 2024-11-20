61ea60b5524d603ef4457066c5fc407d9b9b7f23
ifef228 <ifef228@gmail.com>
2024-11-20 19:27:23 +0300

    refactor

--- src/main/java/faang/school/postservice/consumer/KafkaPostConsumer.java
+++ src/main/java/faang/school/postservice/consumer/KafkaPostConsumer.java
@@ -6,16 +6,12 @@ import faang.school.postservice.mapper.post.PostPublishedEventMapper;
 import faang.school.postservice.protobuf.generate.PostPublishedEventProto;
 import faang.school.postservice.service.FeedService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class KafkaPostConsumer implements KafkaConsumer<byte[]> {
-    @Value("${spring.kafka.topics.posts.name}")
-    private String topic;
-
     private final PostPublishedEventMapper mapper;
     private final FeedService service;
 
