125ebbf1be78554ea71ffd8ab38146a6bd46c3e5
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-11-22 14:25:19 +0500

    BJS2-40813 Task
    Added LikeController and LikeService.
    
    With Tests (version 6.0)

--- src/main/java/faang/school/postservice/service/LikeService.java
+++ src/main/java/faang/school/postservice/service/LikeService.java
@@ -2,6 +2,8 @@ package faang.school.postservice.service;
 
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.exception.NetworkException;
+import faang.school.postservice.exception.UserNotFoundException;
 import faang.school.postservice.model.Like;
 import faang.school.postservice.repository.LikeRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +25,13 @@ public class LikeService {
     private final UserServiceClient userServiceClient;
 
     public List<UserDto> getUsersWhoLikePostByPostId(long Id) {
-        List<Like> usersWhoLikedPost  = likeRepository.findByPostId(Id);
+        List<Like> usersWhoLikedPost = likeRepository.findByPostId(Id);
         return mapLikesToUserDtos(usersWhoLikedPost);
     }
 
 
     public List<UserDto> getUsersWhoLikeComments(long id) {
-        List<Like> usersWhoLikedComment  = likeRepository.findByCommentId(id);
+        List<Like> usersWhoLikedComment = likeRepository.findByCommentId(id);
         return mapLikesToUserDtos(usersWhoLikedComment);
     }
 
@@ -54,25 +56,29 @@ public class LikeService {
 
     private List<UserDto> fetchUserDtosSafely(List<Long> batch) {
         List<UserDto> userDtos = new ArrayList<>();
+
         for (Long userId : batch) {
-            try {
-                userDtos.add(userServiceClient.getUser(userId));
-            } catch (Exception e) {
-                log.error("Error fetching user with ID {}: {}", userId, e.getMessage(), e);
-            }
+            userDtos.add(userServiceClient.getUser(userId));
         }
+
         return userDtos;
     }
 
     private List<UserDto> collectResultsFromFutures(List<CompletableFuture<List<UserDto>>> futures) {
-        try {
-            return futures.stream()
-                    .map(CompletableFuture::join)
-                    .flatMap(List::stream)
-                    .collect(Collectors.toList());
-        } catch (CompletionException e) {
-            log.error("Error while joining CompletableFuture: {}", e.getMessage(), e);
-            throw new RuntimeException("Error retrieving user DTOs", e.getCause());
+        List<UserDto> collectedResults = new ArrayList<>();
+
+        for (CompletableFuture<List<UserDto>> future : futures) {
+            try {
+                collectedResults.addAll(future.join());
+            } catch (CompletionException e) {
+                log.error("Error while joining CompletableFuture: {}", e.getMessage(), e);
+                throw new RuntimeException("Error retrieving user DTOs", e.getCause());
+            } catch (Exception e) {
+                log.error("Unexpected error while collecting results: {}", e.getMessage(), e);
+                throw new RuntimeException("Unexpected error occurred during the process", e);
+            }
         }
+
+        return collectedResults;
     }
 }
