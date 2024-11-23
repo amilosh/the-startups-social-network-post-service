184b6cddda7c6d69e21f4ead69c4c7ddf4767c8c
ifef228 <ifef228@gmail.com>
2024-11-23 15:51:55 +0300

    refactor

--- src/main/java/faang/school/postservice/mapper/comment/CommentEventMapper.java
+++ src/main/java/faang/school/postservice/mapper/comment/CommentEventMapper.java
@@ -1,17 +1,10 @@
 package faang.school.postservice.mapper.comment;
 
-import com.google.protobuf.Timestamp;
 import faang.school.postservice.dto.comment.CommentEvent;
 import faang.school.postservice.mapper.DateTimeMapper;
 import faang.school.postservice.protobuf.generate.CommentEventProto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 
 /**
