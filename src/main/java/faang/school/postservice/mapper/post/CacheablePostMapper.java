dbd160d9b035fe57f34f9e39ded07401b29a8be2
ifef228 <ifef228@gmail.com>
2024-11-23 11:56:14 +0300

    mapper

--- src/main/java/faang/school/postservice/mapper/post/CacheablePostMapper.java
+++ src/main/java/faang/school/postservice/mapper/post/CacheablePostMapper.java
@@ -3,29 +3,30 @@ package faang.school.postservice.mapper.post;
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.mapper.comment.CommentMapper;
 import faang.school.postservice.model.Comment;
+import faang.school.postservice.model.Like;
 import faang.school.postservice.model.post.CacheablePost;
 import faang.school.postservice.model.post.Post;
-import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-@RequiredArgsConstructor
 public abstract class CacheablePostMapper {
     @Value("${feed.cache.post.time-to-live-in-seconds}")
     private long timeToLive;
 
-    private final CommentMapper commentMapper;
+    @Autowired
+    private CommentMapper commentMapper;
 
-    @Mapping(target = "countOfLikes", source = "likes.size()")
-    @Mapping(target = "countOfComments", source = "comments.size()")
+    @Mapping(target = "countOfLikes", source = "likes", qualifiedByName = "getListSizeLike")
+    @Mapping(target = "countOfComments", source = "comments", qualifiedByName = "getListSizeComment")
     @Mapping(target = "comments", source = "comments", qualifiedByName = "commentsMap")
-    @Mapping(target = "timeToLive", qualifiedByName = "timeToLive")
+    @Mapping(target = "timeToLive", source = "comments", qualifiedByName = "timeToLive")
     public abstract CacheablePost toCacheablePost(Post post);
 
     @Named("commentsMap")
@@ -37,7 +38,17 @@ public abstract class CacheablePostMapper {
     }
 
     @Named("timeToLive")
-    protected long getTimeToLive() {
+    protected long getTimeToLive(List<Comment> comments) {
         return timeToLive;
+    }
+
+    @Named("getListSizeLike")
+    protected long getListSizeLike(List<Like> list) {
+        return list.size();
+    }
+
+    @Named("getListSizeComment")
+    protected long getListSizeComment(List<Comment> list) {
+        return list.size();
     }
 }
