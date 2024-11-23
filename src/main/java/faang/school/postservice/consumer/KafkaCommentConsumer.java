44ef66b438dca3f593fd19d4ba06e27a5fe087a8
ifef228 <ifef228@gmail.com>
2024-11-23 16:11:00 +0300

    refactor

--- src/main/java/faang/school/postservice/consumer/KafkaCommentConsumer.java
+++ src/main/java/faang/school/postservice/consumer/KafkaCommentConsumer.java
@@ -1,11 +1,8 @@
 package faang.school.postservice.consumer;
 
 import com.google.protobuf.InvalidProtocolBufferException;
-import faang.school.postservice.dto.comment.CommentEvent;
 import faang.school.postservice.dto.comment.CommentPublishedEvent;
-import faang.school.postservice.mapper.comment.CommentEventMapper;
 import faang.school.postservice.mapper.comment.CommentPublishedEventMapper;
-import faang.school.postservice.protobuf.generate.CommentEventProto;
 import faang.school.postservice.protobuf.generate.CommentPublishedEventProto;
 import faang.school.postservice.service.FeedService;
 import lombok.RequiredArgsConstructor;
