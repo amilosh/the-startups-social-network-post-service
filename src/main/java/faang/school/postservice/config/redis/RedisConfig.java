57a11b7eeb776e4b52933f5f646a73f7cccbcd23
ifef228 <ifef228@gmail.com>
2024-11-23 10:41:44 +0300

    post moved

--- src/main/java/faang/school/postservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfig.java
@@ -1,6 +1,6 @@
 package faang.school.postservice.config.redis;
 
-import faang.school.postservice.model.Post;
+import faang.school.postservice.model.post.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
