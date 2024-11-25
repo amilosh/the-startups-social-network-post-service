bcf123b678d19e0c681c9d7ebc329d2598b08d7f
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-11-25 18:29:25 +0300

    test

---
+++ src/test/java/faang/school/postservice/CiTest.java
@@ -0,0 +1,13 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CiTest {
+
+
+    @Test
+    void test() {
+        Assertions.fail();
+    }
+}
