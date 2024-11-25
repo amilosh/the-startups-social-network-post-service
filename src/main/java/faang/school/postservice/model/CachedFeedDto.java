4ea29ad2c9a3b92aaee2e50ca2443a1820b2f2c9
Ilya Merkulov <imerkulov@fun.co>
2024-11-25 17:29:25 +0200

    BJS2-43430 Эндпоинт получения фида пользователя

--- src/main/java/faang/school/postservice/model/CachedFeedDto.java
+++ src/main/java/faang/school/postservice/model/CachedFeedDto.java
@@ -1,9 +1,9 @@
 package faang.school.postservice.model;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
