48051e548ec645cda997b54fb89033adab3be731
Sergey Sklyar <sklyar1091@gmail.com>
2024-11-25 20:11:57 +0300

    - unused imports

--- src/main/java/faang/school/postservice/producer/like/comment/KafkaCommentLikeProducer.java
+++ src/main/java/faang/school/postservice/producer/like/comment/KafkaCommentLikeProducer.java
@@ -3,7 +3,6 @@ package faang.school.postservice.producer.like.comment;
 import faang.school.postservice.config.properties.kafka.KafkaProperties;
 import faang.school.postservice.event.kafka.comment.like.CommentLikeKafkaEvent;
 import faang.school.postservice.producer.AbstractEventProducer;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
--- src/main/java/faang/school/postservice/producer/like/post/KafkaPostLikeProducer.java
+++ src/main/java/faang/school/postservice/producer/like/post/KafkaPostLikeProducer.java
@@ -3,7 +3,6 @@ package faang.school.postservice.producer.like.post;
 import faang.school.postservice.config.properties.kafka.KafkaProperties;
 import faang.school.postservice.event.kafka.post.like.PostLikeKafkaEvent;
 import faang.school.postservice.producer.AbstractEventProducer;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
