fe84a8d5a4d579f1210a7836540abdf68c0fd964
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-11-25 10:25:59 +0500

    BJS2-40813 Task
    Added LikeController and LikeService.
    
    With Tests (version 15.0)

--- src/main/java/faang/school/postservice/client/UserServiceClient.java
+++ src/main/java/faang/school/postservice/client/UserServiceClient.java
@@ -4,7 +4,6 @@ import faang.school.postservice.dto.user.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -14,6 +13,6 @@ public interface UserServiceClient {
     @GetMapping("/api/v1/users/{userId}")
     UserDto getUser(@PathVariable long userId);
 
-    @PostMapping("/api/v1/users")
+    @GetMapping("/api/v1/users")
     List<UserDto> getUsersByIds(@RequestBody List<Long> ids);
 }
