e53d12c3ee38df6433b3f80dd26016d85a146317
Viktor Krivetskiy <1woracc@gmail.com>
2024-11-26 21:09:18 +0300

    Валидация в сервисе Постов

--- src/main/java/faang/school/postservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/postservice/dto/project/ProjectDto.java
@@ -1,9 +1,16 @@
 package faang.school.postservice.dto.project;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 public class ProjectDto {
     private long id;
+
+    @NotBlank(message = "Title should not be blank")
+    @Size(max = 255, message = "Title should not exceed 255 characters")
     private String title;
 }
--- src/main/java/faang/school/postservice/dto/user/UserDto.java
+++ src/main/java/faang/school/postservice/dto/user/UserDto.java
@@ -1,16 +1,24 @@
 package faang.school.postservice.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Validated
 public class UserDto {
     private Long id;
+
+    @NotBlank(message = "Username should not be blank")
+    @Size(max = 255, message = "Username should not exceed 255 characters")
     private String username;
+
+    @NotBlank(message = "Email should not be blank")
+    @Email(message = "Email should be valid")
+    @Size(max = 255, message = "Email should not exceed 255 characters")
     private String email;
 }
