451ec9d8e6daca092bc6519c0bfe79d46ff57664
SerUKalinin <serukalinin@gmail.com>
2024-11-26 11:17:49 +0300

    src/main/java/faang/school/postservice/config/async/AsyncConfig.java

--- src/main/java/faang/school/postservice/config/async/AsyncConfig.java
+++ src/main/java/faang/school/postservice/config/async/AsyncConfig.java
@@ -1,6 +1,6 @@
 package faang.school.postservice.config.async;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
