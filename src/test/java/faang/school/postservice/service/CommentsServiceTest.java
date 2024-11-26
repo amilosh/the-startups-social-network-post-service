258ed6f8c166f7b0efbea71d5986c121c2b602ac
Mark Janovskij <markyanovskiy@gmail.com>
2024-11-27 00:03:10 +0100

    Work without Unit test+ CommentControl Tests fix2+Comment Service Test

--- src/test/java/faang/school/postservice/service/CommentsServiceTest.java
+++ src/test/java/faang/school/postservice/service/CommentsServiceTest.java
@@ -1,4 +1,60 @@
 package faang.school.postservice.service;
 
+import faang.school.postservice.dto.CommentDto;
+import faang.school.postservice.mapper.CommentMapper;
+import faang.school.postservice.model.Comment;
+import faang.school.postservice.repository.CommentRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+@ExtendWith(MockitoExtension.class)
 public class CommentsServiceTest {
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private CommentMapper commentMapper;
+    @InjectMocks
+    private CommentService commentService;
+
+    @Test
+    void createComment_Success(){
+
+    }
+    @Test
+    void updateComment_Success(){
+
+    }
+    @Test
+    void getAllComments_Success(){
+
+    }
+    @Test
+    void deleteComment_Success(){
+
+    }
+    private CommentDto creatTestCommentDto(){
+        return CommentDto.builder()
+                .id(1L)
+                .content("Test comment")
+                .authorId(2L)
+                .postId(100L)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+    private Comment creatTestComment(){
+        return Comment.builder()
+                .id(1L)
+                .content("Test comment")
+                .authorId(2L)
+
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
 }
