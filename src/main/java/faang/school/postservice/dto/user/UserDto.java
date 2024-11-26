e43780697f2211f3aff08e0f5a454edc7c9d8cbd
Viktor Krivetskiy <1woracc@gmail.com>
2024-11-26 21:13:52 +0300

    Валидация в сервисе Постов -fix

--- src/main/java/faang/school/postservice/dto/user/UserDto.java
+++ src/main/java/faang/school/postservice/dto/user/UserDto.java
@@ -4,11 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Validated
 public class UserDto {
     private Long id;
