2fcb111b118419f1ac3b69997d5ddb71b2cd8493
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-11-29 17:50:52 +0300

    BJS2-40975 Валидация в сервисе Постов

--- src/main/java/faang/school/postservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/postservice/dto/project/ProjectDto.java
@@ -2,6 +2,7 @@ package faang.school.postservice.dto.project;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,5 +10,6 @@ public class ProjectDto {
     @Min(value = 1, message = "ID must be a positive number")
     private long id;
     @NotBlank(message = "Title should not be blank")
+    @Size(max = 255, message = "Title should not exceed 255 characters")
     private String title;
 }
