a27252751c5ec315bd9b612da60df94aed8f6029
Denis Artamonov <dns.artamonov@gmail.com>
2024-12-02 11:29:41 +0300

    BJS2-43455-DD refactoring

--- src/main/java/faang/school/postservice/config/kafka/KafkaProducerConfig.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaProducerConfig.java
@@ -1,5 +1,7 @@
 package faang.school.postservice.config.kafka;
 
+import faang.school.postservice.util.KafkaProducerConstants;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,8 +15,10 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @Configuration
 public class KafkaProducerConfig {
+    private final KafkaProducerConstants kafkaProduceConstants;
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
@@ -25,12 +29,11 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(JsonSerializer.TYPE_MAPPINGS,
-                "comment_event:faang.school.postservice.model.event.kafka.CommentEventKafka, " +
-                        "post_event:faang.school.postservice.model.event.kafka.PostEventKafka, " +
-                        "post_view_event:faang.school.postservice.model.event.kafka.PostViewEventKafka, " +
-                        "heat_task_event:faang.school.postservice.model.event.kafka.HeatTaskEventKafka, " +
-                        "like_event:faang.school.postservice.model.event.kafka.PostLikeEventKafka");
+        configProps.put(JsonSerializer.TYPE_MAPPINGS, kafkaProduceConstants.getCOMMENT_EVENT()
+                + kafkaProduceConstants.getPOST_EVENT()
+                + kafkaProduceConstants.getPOST_VIEW_EVENT()
+                + kafkaProduceConstants.getHEAT_TASK_EVEN()
+                + kafkaProduceConstants.getLIKE_EVENT());
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
