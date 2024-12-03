ad21f8ef9768a9d0e1c3c4fd9c80c8d7108fe9d5
Maksim <15maksim@mail.ru>
2024-12-03 15:23:12 +0300

    Update PostService.java

--- src/main/java/faang/school/postservice/service/post/PostService.java
+++ src/main/java/faang/school/postservice/service/post/PostService.java
@@ -1,7 +1,6 @@
 package faang.school.postservice.service.post;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.json.dto.DtoBanSchema;
 import faang.school.postservice.client.ProjectServiceClient;
 import faang.school.postservice.client.UserServiceClient;
@@ -77,7 +76,6 @@ public class PostService {
     private final ObjectMapper objectMapper;
     private final GingerCorrector gingerCorrector;
     private final MessageSenderForUserBanImpl messageSenderForUserBan;
-    private final ObjectMapper objectMapper;
 
     @Value("${size.not-verified-posts-for-users}")
     private int sizeNotVerifiedPostsForUsers;
